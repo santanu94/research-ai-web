ec2c928ab74e817e262ce8920c4c126997e19310
Santanu Bhattacharjee <mail.santanu94@gmail.com>
2024-06-27 14:23:40 +0530

    updated latest papers in dashboard

--- src/components/Dashboard/LatestPapers/LatestPapers.js
+++ src/components/Dashboard/LatestPapers/LatestPapers.js
@@ -16,44 +16,52 @@ const LatestPapers = () => {
       {
         title: "MoRA: High-Rank Updating for Parameter-Efficient Fine-Tuning",
         id: "2405.12130v1",
+        published: "05/20/2024",
         url: "https://arxiv.org/abs/2405.12130",
       },
       {
         title:
           "How Far Are We to GPT-4V? Closing the Gap to Commercial Multimodal Models with Open-Source Suites",
         id: "2404.16821v2",
+        published: "04/29/2024",
         url: "https://arxiv.org/abs/2404.16821",
       },
       {
         title:
           "Retrieval-Augmented Generation for AI-Generated Content: A Survey",
-        id: "2402.19473v4",
+        id: "2402.19473v6",
+        published: "06/21/2024",
         url: "https://arxiv.org/abs/2402.19473",
       },
       {
         title: "KAN: Kolmogorov-Arnold Networks",
-        id: "2404.19756v3",
+        id: "2404.19756v4",
+        published: "06/16/2024",
         url: "https://arxiv.org/abs/2404.19756",
       },
       {
         title: "MambaOut: Do We Really Need Mamba for Vision?",
         id: "2405.07992v3",
+        published: "05/20/2024",
         url: "https://arxiv.org/abs/2405.07992",
       },
       {
         title: "How Far Are We From AGI",
         id: "2405.10313v1",
+        published: "05/16/2024",
         url: "https://arxiv.org/abs/2405.10313",
       },
     ];
     setLatestPapers(fetchedPapers);
   };
 
-  const openLatestPaper = (id, pdfUrl) => {
+  const openLatestPaper = (id, paperTitle, paperPublishedDate, pdfUrl) => {
     // mixpanel.track_links("#paper-result", "Clicked to Open Paper");
     mixpanel.track("Clicked to Open Latest Paper");
 
-    navigate(`/paper/${id}`, { state: { pdfUrl, referrer: "dashboard" } });
+    navigate(`/paper/${id}`, {
+      state: { paperTitle, paperPublishedDate, pdfUrl, referrer: "dashboard" },
+    });
   };
 
   return (
@@ -64,7 +72,9 @@ const LatestPapers = () => {
           <div
             key={paper.id}
             className="paper"
-            onClick={() => openLatestPaper(paper.id, paper.url)}
+            onClick={() =>
+              openLatestPaper(paper.id, paper.title, paper.published, paper.url)
+            }
           >
             {paper.title}
           </div>
